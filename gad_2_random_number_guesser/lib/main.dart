9955e21ff8e7bde34a0f82e69c7f3d7439b0db13
MateiEne <ene.matei21@gmail.com>
2021-11-10 11:50:31 +0200

    you can now guess a number and get hints

--- gad_2_random_number_guesser/lib/main.dart
+++ gad_2_random_number_guesser/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -25,6 +27,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController controller = TextEditingController();
+
+  int? valueReceived;
+
+  bool isHintTextVisible = false;
+
+  int randomNumberToBeGuessed = Random().nextInt(99) + 1;
+
+  String hintText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,51 +57,109 @@ class _HomePageState extends State<HomePage> {
               textAlign: TextAlign.center,
             ),
             const Padding(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 32),
+              padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
               child: Text(
                 "It's your turn to guess my number!",
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
               ),
             ),
-            Card(
-              elevation: 24.0,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.88,
-                height: MediaQuery.of(context).size.height * 0.28,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      const Expanded(
-                        flex: 3,
-                        child: Text(
-                          'Try a number!',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 32.0,
+            Visibility(
+              child: Column(
+                children: [
+                  Text(
+                    'You tried ${valueReceived.toString()}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 28,
+                    ),
+                  ),
+                  Text(
+                    ' $hintText',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 28,
+                    ),
+                  ),
+                ],
+              ),
+              visible: isHintTextVisible,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+              child: Card(
+                elevation: 24.0,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.88,
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            'Try a number!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 32.0,
+                            ),
                           ),
                         ),
-                      ),
-                      const Expanded(
-                        flex: 2,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            hintText: 'Enter a number',
+                        Expanded(
+                          flex: 2,
+                          child: TextField(
+                            controller: controller,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter a number',
+                            ),
                           ),
                         ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Guess'),
-                      )
-                    ],
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              if (controller.text.isEmpty) {
+                                return;
+                              }
+
+                              valueReceived = int.tryParse(controller.text);
+
+                              if (valueReceived == null) {
+                                print('not a number');
+                                return;
+                              }
+
+                              isHintTextVisible = true;
+
+                              if (valueReceived! < randomNumberToBeGuessed) {
+                                print('try higher');
+                                hintText = 'Try higher!';
+                              } else if (valueReceived! >
+                                  randomNumberToBeGuessed) {
+                                hintText = 'Try lower!';
+                                print('try lower');
+                              } else {
+                                hintText = 'You guessed it!';
+                                print('You guessed it');
+                              }
+                            });
+                          },
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.grey),
+                          ),
+                          child: const Text(
+                            'Guess',
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
